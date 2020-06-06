35f9ed5d38423ffe002cb2420d4ec61f4b631177
Andrej <anhr@mail.ru>
2020-06-06 08:18:19 +0700

    debug version

--- myPoints/myPoints.js
+++ myPoints/myPoints.js
@@ -486,7 +486,7 @@ function loadShaderText ( onload, path ) {
 		//load vertex.c file
 		var vertex_loader = new THREE.FileLoader( THREE.DefaultLoadingManager );
 		vertex_loader.setResponseType( 'text' );
-		vertex_loader.load( vertex_url, function ( _vertex_text ) {
+		vertex_loader.load( vertex_url, function ( vertex_text ) {
 /*
 			vertex_text = _vertex_text;
 			loaded();
@@ -494,7 +494,7 @@ function loadShaderText ( onload, path ) {
 		//load fragment.c file
 			var fragment_loader = new THREE.FileLoader( THREE.DefaultLoadingManager );
 			fragment_loader.setResponseType( 'text' );
-			fragment_loader.load( fragment_url, function ( _fragment_text ) {
+			fragment_loader.load( fragment_url, function ( fragment_text ) {
 
 				onLoad( vertex_text, fragment_text );
 
